3a61948990ff1b302fdeed2d2fcdc96eaa49814c
watosnfan <30586391+watosnfan@users.noreply.github.com>
2021-02-26 20:53:05 +0800

    Add files via upload

---
+++ Ex01.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# for i in range(1,5):
+#     for a in range(3):
+#         print(i,end='')
+#     print()
+
+# print("Finish")    
+
+
+for i in range(1,5):
+    for a in range(1,i+1):
+        print(a,end='')
+    print()
+
+print("Finish")   
---
+++ Ex02.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 19:10:52 2021
+
+@author: USER
+"""
+
+for a in range(2,10):
+    for b in range(1,10):
+        print(a,"x",b,"=",a*b,sep='',end='\t')  # \t is tab 
+    print()    
